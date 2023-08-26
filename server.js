66f26b01cfe73519fd4f50b05670702dd5480c4e
SriSomanaath <govindalapudisrinath@gmail.com>
2023-08-26 10:55:42 +0530

    styles and server communication

--- server.js
+++ server.js
@@ -9,3 +9,13 @@ const PORT = process.env.PORT || 3000
 http.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
 })
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+const io = require('socket.io')(http)
+
